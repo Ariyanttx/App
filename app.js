5fe0fc38d4e4461cfba54136d9a1287eef72b8d9
ariyanttx <ariyanttx@gmail.com>
2018-09-10 20:04:10 +0600

    lecture 7 of twencle casts

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-var a = 5;
-var b = 6;
-var c = a + b;
+// function host() {
+//     console.log('i am host function...');
+//      return function () {
+//     console.log('i am chiled');
+//      }
+// }
+// var a =host();
+// a;
+// console.log(a());
 
-console.log(c);
+
+function greeting(msg){
+    return function (name) {
+        console.log(msg + ' ' + name);
+        
+    }
+}
+
+var good = greeting('Good morning');
+var hello = greeting ('hello');
+
+hello('ibrahim');
+good ('toki ami ki');
