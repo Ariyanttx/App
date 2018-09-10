c79b35d66a4abec2a5f24fb7b1d107d401fcadd7
ariyanttx <ariyanttx@gmail.com>
2018-09-10 19:11:30 +0600

    i just add app dot-js and ceate a code

---
+++ app2.js
@@ -0,0 +1,27 @@
+// var arr = [4, 5, 1, 0, 8];
+// arr.forEach(function(a) {
+//     console.log(a);
+    
+// });
+
+
+// function hello (name) {
+//     print (name);
+//     print ('something else')
+// }
+
+// function print (args) {
+//     console.log(args);
+// }
+
+// hello('ibrahim');
+
+function hello (name, print) {
+    print(name);
+}
+
+// hello('ibrahim', print);
+
+//  function  print(abc) {
+//      console.log(abc);
+//  }
